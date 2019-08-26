a2685f1397de77c13103ce1c806a18adc0e2159a
flyntga <austin.flynt@jacobs.com>
2019-08-26 09:16:15 -0500

    output wrs in reverse order

--- ff.py
+++ ff.py
@@ -50,55 +50,43 @@ print("plotting file: " + filename1)
 
 for player in range(len(data)):
   posrank = int(data[player][0])
-  allrank = data[player][1]
+  allrank = int(data[player][1].rstrip(')').lstrip(' ('))
   name = data[player][2]
-  team = data[player][3]
-  price = data[player][4]
-  bye = data[player][5]
-  # print(data[data[player]][6])
-  # print('player: ',name,'has ranks: ',posrank,allrank,' on team ',team,' and costs:',price)
-  print(posrank,allrank,price,team,name)
-#01, (043) , Patrick Mahomes   , KC  , $18,  12 
+  team = data[player][3].rstrip().lstrip()
+  price = int(data[player][4].lstrip(' $').rstrip())
+  bye = int(data[player][5].lstrip().rstrip())
+  if(bye == 11):
+    onBoard = False
+  else:
+    onBoard = True
 
-#plot the data
-#for i in range(1,ncol):
-  #plt.plot(data[:,0], data[:,i], label=headers[i])
+  if posrank == 73:
+    onBoard = False
+  data[player][0] = posrank
+  data[player][1] = allrank
+  data[player][3] = team
+  data[player][4] = price
+  data[player][5] = bye
+  data[player].append(onBoard)
+  if not onBoard:
+    print(data[player])
 
-#if twoFiles:
-#  ncol=d2.shape[1]
-#  for i in range(1,ncol):
-#    plt.plot(d2[:,0], d2[:,i], '--', label=h2[i])
+def showboard(choice):
+  with open('output.dat', 'w') as f:
+    for player in reversed(range(len(data))):
+      p = data[player]
+      if p[0] == choice:
+        print("removing player",data[player])
+        data[player][-1] = False
+        p = data[player]
 
+      # Print player if on board
+      if p[-1]:
+        f.write('{}\n'.format(p))
+        #f.write('\n'.join('{}'.format(p)))
 
-# curve smoothing
-#for i in range(1,ncol):
-#  fi = savgol_filter(data[:,i], 51, 3) # window size 51, polynomial order 3
-#  plt.plot(data[:,0], fi, label=headers[i]+'-fit')
-
-#plt.xlabel('Time (s)')
-#plt.ylabel(headers[1])
-#plt.xlim(1.1,2.1)
-#plt.xticks([1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1])
-#plt.grid(True)
-#plt.legend()
-##plt.savefig('latest.png')
-#ofile = filename1.replace("csv", "png")
-#ofile = ofile.replace("dat", "png")
-#plt.savefig(ofile)
-#plt.show()
-print(" ")
-
-"""
-# SAVE DATA TO FILE
-#with open('output.dat', 'w') as f:
-#  f.write('\n'.join('{} {} {}'.format(t[0], t[1], t[2]) for t in zip(r0, r1, f1)))
-#  f.close()
-
-# MANUAL PLOTTING
-#plt.ylim(-4,4)
-plt.ylabel(r'$Forces$ $(N)$')
-plt.title('Global Forces')
-plt.plot(r0,r1  ,'-', label=r'$F_x (N)$')
-plt.plot(r0,r2  ,'-', label=r'$F_y (N)$')
-plt.plot(r0,r3  ,'-', label=r'$F_z (N)$')
-"""
+while True:
+  choice = input("who to remove?")
+  choice = int(choice)
+  print("removing #",choice)
+  showboard(choice)
