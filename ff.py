6fe818559e31241f2702c80c71576762f600fdfb
flyntga <austin.flynt@jacobs.com>
2019-08-26 11:02:44 -0500

    can pull in all positions

--- ff.py
+++ ff.py
@@ -10,6 +10,9 @@ csv.register_dialect(\
     'mydialect', delimiter = ',',quotechar = '"', doublequote = True, \
     skipinitialspace = True, lineterminator = '\n' )
 
+def sort_allrank(val):
+  return val[1]
+
 def read_file(filename):
   with open(filename, 'r') as f:
     reader = csv.reader(f, delimiter=',')
@@ -17,17 +20,19 @@ def read_file(filename):
     # get all the rows as a list
     data = list(reader)
 
+    for i in range(len(data)):
+      data[i].append(filename.rstrip('.txt').lstrip('pos_'))
+
     return data
 
 twoFiles = False
-
 #filename1 = 'names.csv'
-#filename1 = 'pos_d.txt'
-#filename1 = 'pos_k.txt'
-#filename1 = 'pos_qb.txt'
-#filename1 = 'pos_rb.txt'
-#filename1 = 'pos_te.txt'
-filename1 = 'pos_wr.txt'
+f1 = 'pos_d.txt'
+f2 = 'pos_k.txt'
+f3 = 'pos_qb.txt'
+f4 = 'pos_rb.txt'
+f5 = 'pos_te.txt'
+f6 = 'pos_wr.txt'
 if len(sys.argv) == 2:
   filename1 = sys.argv[1]
   data = read_file(filename1)
@@ -35,52 +40,52 @@ if len(sys.argv) == 3:
   twoFiles = True
   filename1 = sys.argv[1]
   filename2 = sys.argv[2]
-  headers, data = read_file(filename1)
-  d2 = read_file(filename2)
-else:
   data = read_file(filename1)
+  d2 = read_file(filename2)
+  data = data + d2
+else:
+  data = read_file(f1)
+  d2   = read_file(f2)
+  d3   = read_file(f3)
+  d4   = read_file(f4)
+  d5   = read_file(f5)
+  d6   = read_file(f6)
+  data = data+d2+d3+d4+d5+d6
 
-print("plotting file: " + filename1)
+# combine positions for all players
 
-#print(headers)
-#print(data.shape)
-#ncol=data.shape[1]
-#nrow=data.shape[0]
-#print(data[:3])
+# Sort the data based on allrank
+data.sort(key = sort_allrank)
 
+# add onBoard Field to player
 for player in range(len(data)):
   posrank = int(data[player][0])
-  allrank = int(data[player][1].rstrip(')').lstrip(' ('))
+  allrank = int(data[player][1].rstrip(') ').lstrip(' ('))
   name = data[player][2]
   team = data[player][3].rstrip().lstrip()
   price = int(data[player][4].lstrip(' $').rstrip())
   bye = int(data[player][5].lstrip().rstrip())
-  if(bye == 11):
-    onBoard = False
-  else:
-    onBoard = True
-
-  if posrank == 73:
-    onBoard = False
+  onBoard = True
   data[player][0] = posrank
   data[player][1] = allrank
   data[player][3] = team
   data[player][4] = price
   data[player][5] = bye
   data[player].append(onBoard)
-  if not onBoard:
-    print(data[player])
 
+# show all players on board
 def showboard(choice):
   with open('output.dat', 'w') as f:
     for player in reversed(range(len(data))):
       p = data[player]
-      if p[0] == choice:
-        print("removing player",data[player])
+
+      # remove player matching choice in allrank
+      if p[1] == choice:
+        print("removing player",p)
         data[player][-1] = False
         p = data[player]
 
-      # Print player if on board
+      # Print player if still on board
       if p[-1]:
         f.write('{}\n'.format(p))
         #f.write('\n'.join('{}'.format(p)))
